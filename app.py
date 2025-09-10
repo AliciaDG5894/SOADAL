a6081f33f89aa103decc5dbaca5344e84d92774d
Adrian506-coder <treviadrian18@gmail.com>
2025-09-10 13:20:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ from flask_cors import CORS, cross_origin
 
 con = mysql.connector.connect(
     host="185.232.14.52",
-    database="u760464709_16005339_bd",
-    user="u760464709_16005339_usr",
-    password="/iJRzrJBz+P1"
+    database="u760464709_23005256_bd",
+    user="u760464709_23005256_usr",
+    password="~6ru!MMJZzX"
 )
 
 app = Flask(__name__)
@@ -31,15 +31,14 @@ def pusherProductos():
     import pusher
     
     pusher_client = pusher.Pusher(
-      app_id="2046005",
-      key="e57a8ad0a9dc2e83d9a2",
-      secret="8a116dd9600a3b04a3a0",
-      cluster="us2",
+      app_id='2046097',
+      key='1007852abe277cd3e121',
+      secret='7c6cff8082dfcc0b42a9',
+      cluster='us2',
       ssl=True
     )
     
-    pusher_client.trigger("canalProductos", "eventoProductos", {"message": "Hola Mundo!"})
-    return make_response(jsonify({}))
+    pusher_client.trigger("canalTrajes", "eventoTrajes", {"message": "Hola Mundo!"})
 
 @app.route("/")
 def index():
@@ -90,6 +89,93 @@ def iniciarSesion():
 def productos():
     return render_template("productos.html")
 
+@app.route("/trajes")
+def trajes():
+    return render_template("trajes.html")
+
+@app.route("/tbodyTrajes")
+def tbodyTrajes():
+    if not con.is_connected():
+        con.reconnect()
+    
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT IdTraje,
+           nombreTraje,
+           descripcion
+
+    FROM trajes
+
+    ORDER BY IdTraje DESC
+
+    LIMIT 10 OFFSET 0
+    """
+
+    cursor.execute(sql)
+    registros = cursor.fetchall()
+
+    # Si manejas fechas y horas
+    """
+    for registro in registros:
+        fecha_hora = registro["Fecha_Hora"]
+
+        registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+        registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+        registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
+    """
+
+    return render_template("tbodyTrajes.html", trajes=registros)
+
+@app.route("/trajes/guardar", methods=["POST", "GET"])
+def guardarTraje():
+    if not con.is_connected():
+        con.reconnect()
+
+    if request.method == "POST":
+        data = request.get_json() or request.form
+        nombre = data.get("txtNombre")
+        descripcion = data.get("txtDescripcion")
+    else: 
+        nombre = request.args.get("nombre")
+        descripcion = request.args.get("descripcion")
+    if not nombre or not descripcion:
+        return jsonify({"error": "Faltan parámetros"}), 400
+        
+    cursor = con.cursor()
+    sql = """
+    INSERT INTO trajes (nombreTraje, descripcion)
+    VALUES (%s, %s)
+    """
+    cursor.execute(sql, (nombre, descripcion))
+    con.commit()
+    con.close()
+
+    pusherProductos()
+
+    return make_response(jsonify({"mensaje": "Traje guardado correctamente"}))
+
+@app.route("/trajes/eliminar", methods=["POST", "GET"])
+def eliminartraje():
+    if not con.is_connected():
+        con.reconnect()
+
+    if request.method == "POST":
+        IdTraje = request.form.get("id")
+    else:
+        IdTraje = request.args.get("id")
+
+    cursor = con.cursor()
+    sql = "DELETE FROM trajes WHERE IdTraje = %s"
+    val = (IdTraje,)
+
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    pusherProductos()
+
+    return make_response(jsonify({"status": "ok"}))
+    
 @app.route("/tbodyProductos")
 def tbodyProductos():
     if not con.is_connected():
@@ -275,3 +361,4 @@ def eliminarProducto():
     con.close()
 
     return make_response(jsonify({}))
+
